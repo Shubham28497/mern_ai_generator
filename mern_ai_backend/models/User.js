a7d72ff9a960db4cff31b085dd549b0db8a1063d
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-26 09:51:30 +0530

    completed user schema

--- mern_ai_backend/models/User.js
+++ mern_ai_backend/models/User.js
@@ -1,31 +1,53 @@
-const mongoose= require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    trailActive: {
+      type: Boolean,
+      required: true,
+    },
+    trailExpires: {
+      type: Date,
+    },
+    subscription: {
+      type: String,
+      enum: ["Trail", "Free", "Basic", "Premium"],
+    },
+    apiRequestCount: {
+      type: Number,
+      default: 0,
+    },
+    monthlyRequestCount: {
+      type: Number,
+      default: 0,
+    },
+    nextBillingDate: Date,
+    payments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Payment",
+      },
+    ],
+    history: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "History",
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  trailActive: {
-    type: Boolean,
-    required: true,
-  },
-  trailExpires: {
-    type: Date,
-  },
-  subscription: {
-    type: String,
-    enum: ["Trail", "Free", "Basic", "Premium"],
-  },
-  apiRequestCount: {
-    type: Number,
-    default:0,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
