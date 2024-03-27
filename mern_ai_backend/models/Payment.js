dc89e192a402243962c073ba910e296c28d0b270
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-27 09:52:53 +0530

    create payment model

---
+++ mern_ai_backend/models/Payment.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    reference: {
+      type: String,
+      required: true,
+    },
+    currency: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "pending",
+      required: true,
+    },
+    subscriptionPlan: {
+      type: String,
+      required:true
+    },
+    amount: {
+      type: Number,
+      default: 0,
+    },
+    monthlyRequestCount: {
+      type: Number,
+      required:true
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//! compile to form the model
+const Payment = mongoose.mongo("Payment", paymentSchema);
+module.exports = Payment;
