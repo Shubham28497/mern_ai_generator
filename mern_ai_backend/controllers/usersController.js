7bcf976f6e75fef1e5d35845caac99aa07b7ba31
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-28 11:40:24 +0530

    validate req body

--- mern_ai_backend/controllers/usersController.js
+++ mern_ai_backend/controllers/usersController.js
@@ -1,10 +1,18 @@
-const User=require("../models/User")
+const User = require("../models/User");
 //*Registration
 const register = (req, res) => {
-  res.json({
-    status: true,
-    message: "Registration was successfull",
-  });
+  try {
+    const { username, email, password } = req.body;
+    //* Validate
+    if(!username||!email||!password){
+        res.status(400)
+        throw new Error("All fields are required")
+    }
+    res.json({
+      status: true,
+      message: "Registration was successfull",
+    });
+  } catch (error) {}
 };
 //*Login
 //*logout
