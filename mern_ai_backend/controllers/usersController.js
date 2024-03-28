46ee343a67725cf5fc220e5ed7f80a500e2dd370
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-28 12:18:42 +0530

    create the user

--- mern_ai_backend/controllers/usersController.js
+++ mern_ai_backend/controllers/usersController.js
@@ -1,23 +1,29 @@
-const bcrypt=require("bcryptjs")
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 //*Registration
 const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
     //* Validate
-    if(!username||!email||!password){
-        res.status(400)
-        throw new Error("All fields are required")
+    if (!username || !email || !password) {
+      res.status(400);
+      throw new Error("All fields are required");
     }
     //* check the email is taken
-    const userExists=await User.findOne({email})
-    if(userExists){
-        res.status(400)
-        throw new Error("User already exists")
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists");
     }
     //* hash the user pass
-    const salt=await bcrypt.genSalt(10)
-    const hashedPassword=await bcrypt.hash(password,salt)
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    //* new user
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
     res.json({
       status: true,
       message: "Registration was successfull",
