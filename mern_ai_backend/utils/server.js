d155af598bfee7fe650ee4a94dee8f7aa8396fa8
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-28 11:15:25 +0530

    add user router

--- mern_ai_backend/utils/server.js
+++ mern_ai_backend/utils/server.js
@@ -1,6 +1,10 @@
 const express= require("express")
+const userRouter = require("../routes/userRouter")
 const app=express()
 const PORT= process.env.PORT|| 5000
+
+//*Routes
+app.use("/api/v1/users",userRouter)
 
 //* start the server
 app.listen(PORT,console.log(`Server is running on port ${PORT}`))
