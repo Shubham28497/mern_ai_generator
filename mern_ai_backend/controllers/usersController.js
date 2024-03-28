1502bf1ddb83d0e7c7cb60c4b48ed55a0139af7d
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-28 11:58:43 +0530

    check the email is taken

--- mern_ai_backend/controllers/usersController.js
+++ mern_ai_backend/controllers/usersController.js
@@ -1,12 +1,18 @@
 const User = require("../models/User");
 //*Registration
-const register = (req, res) => {
+const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
     //* Validate
     if(!username||!email||!password){
         res.status(400)
         throw new Error("All fields are required")
+    }
+    //* check the email is taken
+    const userExists=await User.findOne({email})
+    if(userExists){
+        res.status(400)
+        throw new Error("User already exists")
     }
     res.json({
       status: true,
