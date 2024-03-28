18d03548a02090bfb26337f2444a886998246475
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-28 11:14:41 +0530

    create user controller

--- mern_ai_backend/controllers/usersController.js
+++ mern_ai_backend/controllers/usersController.js
@@ -1,5 +1,14 @@
 //*Registration
+const register = (req, res) => {
+  res.json({
+    status: true,
+    message: "Registration was successfull",
+  });
+};
 //*Login
 //*logout
 //*Profile
 //*Check user auth status
+module.exports = {
+  register,
+};
