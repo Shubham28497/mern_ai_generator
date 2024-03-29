fb1783898443dff878e367eb99da648d7d7e916e
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-29 10:43:16 +0530

    create mongodb file

---
+++ mern_ai_backend/utils/connectDb.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("connection string");
+    console.log(`Mongodb Connected ${conn.connection.host}`)
+} catch (error) {
+    console.log(`Error connecting to MongoDB ${error.message}`)
+    process.exit()
+}
+};
+module.exports=connectDB
