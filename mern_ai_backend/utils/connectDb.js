98ecd2a6665d79b0c192a5d0e2c0f853af35b29f
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-29 11:34:16 +0530

    update connect db dile

--- mern_ai_backend/utils/connectDb.js
+++ mern_ai_backend/utils/connectDb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017");
+    const conn = await mongoose.connect("mongodb://localhost:27017/mern-ai");
     console.log(`Mongodb Connected ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error connecting to MongoDB ${error.message}`);
