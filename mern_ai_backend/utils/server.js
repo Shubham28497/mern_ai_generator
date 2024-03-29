32a38d0ff8e9ed1ce8d53c3ce4b8a8c733c59848
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-29 12:21:47 +0530

    add err handler middleware"

--- mern_ai_backend/utils/server.js
+++ mern_ai_backend/utils/server.js
@@ -5,9 +5,11 @@ const { errorHandler } = require("../middlewares/errorMiddlewares");
 const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
+//* middlewares
 app.use(express.json());
 //*Routes
 app.use("/api/v1/users", userRouter);
-
+//* error handler middlewares
+app.use(errorHandler)
 //* start the server
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
