327c1b8b8c75f62b02e80bf7f6d83581ecc27a51
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-29 11:03:20 +0530

    correct the file

--- mern_ai_backend/utils/connectDb.js
+++ mern_ai_backend/utils/connectDb.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect("connection string");
-    console.log(`Mongodb Connected ${conn.connection.host}`)
-} catch (error) {
-    console.log(`Error connecting to MongoDB ${error.message}`)
-    process.exit()
-}
+    console.log(`Mongodb Connected ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error connecting to MongoDB ${error.message}`);
+    process.exit();
+  }
 };
-module.exports=connectDB
+module.exports = connectDB;
