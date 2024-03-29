32fc44c7a9b6a5ca3d1b1f480a87787d8c53ccf1
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-29 11:01:48 +0530

    add mongodb connect

--- mern_ai_backend/utils/server.js
+++ mern_ai_backend/utils/server.js
@@ -1,10 +1,11 @@
-const express= require("express")
-const userRouter = require("../routes/userRouter")
-const app=express()
-const PORT= process.env.PORT|| 5000
-
+const express = require("express");
+const userRouter = require("../routes/userRouter");
+const connectDB = require("./connectDb");
+const app = express();
+const PORT = process.env.PORT || 5000;
+connectDB();
 //*Routes
-app.use("/api/v1/users",userRouter)
+app.use("/api/v1/users", userRouter);
 
 //* start the server
-app.listen(PORT,console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
