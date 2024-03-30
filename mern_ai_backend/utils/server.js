50dcded518c592e6e090fb9c5d2e3888274671f4
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-30 10:28:44 +0530

    updated file

--- mern_ai_backend/utils/server.js
+++ mern_ai_backend/utils/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config();
+require("dotenv").config()
 
 const userRouter = require("../routes/userRouter");
 const connectDB = require("./connectDb");
