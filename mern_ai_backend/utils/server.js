e23b7aaa53687443c484380af4437fe4ad4040ec
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-30 09:41:37 +0530

    updated server

--- mern_ai_backend/utils/server.js
+++ mern_ai_backend/utils/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+require("dotenv").config();
+
 const userRouter = require("../routes/userRouter");
 const connectDB = require("./connectDb");
 const { errorHandler } = require("../middlewares/errorMiddlewares");
@@ -10,6 +12,6 @@ app.use(express.json());
 //*Routes
 app.use("/api/v1/users", userRouter);
 //* error handler middlewares
-app.use(errorHandler)
+app.use(errorHandler);
 //* start the server
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
