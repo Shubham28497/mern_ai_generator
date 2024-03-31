410440e83aa2a486a42b9d1e3cbeaaf260dbf2ba
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-31 10:46:58 +0530

    add login router

--- mern_ai_backend/routes/userRouter.js
+++ mern_ai_backend/routes/userRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { register } = require("../controllers/usersController");
+const { register,login } = require("../controllers/usersController");
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
+userRouter.post("/login", login);
 
 module.exports = userRouter;
