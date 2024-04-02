de575c67bc4e84f6709339c83a422bbcdd7956fb
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-04-02 14:25:36 +0530

    verify the token

--- mern_ai_backend/middlewares/isAuthenticated.js
+++ mern_ai_backend/middlewares/isAuthenticated.js
@@ -1,8 +1,13 @@
-const asyncHandler=require("express-async-handler")
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const isAuthenticated = asyncHandler(async (req, res, next) => {
+  if (req.cookies.token) {
+//* verify the token
+const decoded=jwt.verify(req.cookies.token,process.env.JWT_SECRET) // the actual login user
+console.log(decoded)
+} else {
+    return res.status(401).json({ message: "Not authorized,no token" });
+  }
+});
 
-const isAuthenticated=asyncHandler(async(req,res,next)=>{
-    console.log(req.cookies,"isAuthenticated")
-    next();
-})
-
-module.exports=isAuthenticated
+module.exports = isAuthenticated;
