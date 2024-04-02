7794a7942ee7d0745bfe34a8c618a1cd19327e4a
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-04-02 15:00:31 +0530

    call api and some params

--- mern_ai_backend/controllers/openAIController.js
+++ mern_ai_backend/controllers/openAIController.js
@@ -1,12 +1,17 @@
-const asyncHandler= require("express-async-handler")
-const axios=require("axios")
+const asyncHandler = require("express-async-handler");
+const axios = require("axios");
 
 //* OpenAI Controller
 
-const openAIController=asyncHandler(async(req,res)=>{
-    try {
+const openAIController = asyncHandler(async (req, res) => {
+  const { prompt } = req.body;
+  try {
+    await axios.post("https://api.openai.com/v1/completions", {
+      model: "gpt-3.5-turbo",
+      prompt,
+      max_tokens: 10,
+    },{
         
-    } catch (error) {
-        
-    }
-})
+    });
+  } catch (error) {}
+});
