f1e7a3c0cfbe5601acd29e0c9f35130d0546abbd
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-04-02 14:56:45 +0530

    create openAI controller file

---
+++ mern_ai_backend/controllers/openAIController.js
@@ -0,0 +1,12 @@
+const asyncHandler= require("express-async-handler")
+const axios=require("axios")
+
+//* OpenAI Controller
+
+const openAIController=asyncHandler(async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+})
