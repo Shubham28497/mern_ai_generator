f34c000efbb7c2a929c938a465e268c61c47c9ca
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-04-02 15:09:28 +0530

    add header and key

--- mern_ai_backend/controllers/openAIController.js
+++ mern_ai_backend/controllers/openAIController.js
@@ -6,12 +6,24 @@ const axios = require("axios");
 const openAIController = asyncHandler(async (req, res) => {
   const { prompt } = req.body;
   try {
-    await axios.post("https://api.openai.com/v1/completions", {
-      model: "gpt-3.5-turbo",
-      prompt,
-      max_tokens: 10,
-    },{
-        
-    });
+    const response=await axios.post(
+      "https://api.openai.com/v1/completions",
+      {
+        model: "gpt-3.5-turbo",
+        prompt,
+        max_tokens: 10,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+            'Content-Type':"application/json"
+        },
+      }
+    );
+    console.log(response)
   } catch (error) {}
 });
+
+module.exports={
+    openAIController
+}
