6ca0aa2736b55eec55ff3f9de727a7bc0659968c
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-04-03 10:01:43 +0530

    updated controller

--- mern_ai_backend/controllers/openAIController.js
+++ mern_ai_backend/controllers/openAIController.js
@@ -1,29 +1,33 @@
-const asyncHandler = require("express-async-handler");
-const axios = require("axios");
-
-//* OpenAI Controller
-
+const asyncHandler=require("express-async-handler")
+const axios=require("axios")
 const openAIController = asyncHandler(async (req, res) => {
   const { prompt } = req.body;
   try {
-    const response=await axios.post(
+    const response = await axios.post(
       "https://api.openai.com/v1/completions",
       {
-        model: "gpt-3.5-turbo",
+        model: "gpt-3.5-turbo-instruct",
         prompt,
         max_tokens: 10,
       },
       {
         headers: {
           Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-            'Content-Type':"application/json"
+          "Content-Type": "application/json",
         },
       }
     );
-    console.log(response)
-  } catch (error) {}
-});
+    const content = response?.data?.choices[0].text?.trim();
 
+    res.status(200).json({
+      status: "success",
+      data: content,
+    });
+  } catch (error) {
+    res.status(429);
+    throw new Error(error);
+  }
+});
 module.exports={
     openAIController
 }
